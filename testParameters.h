e7211fba79d0d44c8cdb1c1bf22bc63bbf21f0ab
joel <joel.haubold@logarithmo.de>
2023-11-06 19:25:44 +0100

    New test params

--- testParameters.h
+++ testParameters.h
@@ -10,9 +10,9 @@
     //Todo: optional struct with three test params to run single
     const boost::optional<unsigned int> PRE_SET_SEED = boost::none;
     const double MAX_WORKSPACE_SIZE = 170;
-    const std::vector<int> WORKSPACE_COMPLEXITY = {50,55,60};
-    const std::vector<int> NMBR_START_POS = {10, 15, 20, 25, 30, 35, 40};
-    const int REPETITIONS = 2;
+    const std::vector<int> WORKSPACE_COMPLEXITY = {40,80,120};
+    const std::vector<int> NMBR_START_POS = {10, 15, 20, 25, 30, 35, 40,45,50,55,60,65,70,75,75,75};
+    const int REPETITIONS = 5;
     const bool USE_WS_SOLVER = true; //Use Well Seprated or Chromatic Solver
     const bool LIMIT_TO_ONE_FS_COMPONENT = false; // Enforce the free space consisting of one connected component
 
